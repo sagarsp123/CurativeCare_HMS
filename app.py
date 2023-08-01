714602eb149ee21ef80b6f3893ed7d470b5168e5
Sagar Prabhu <48390647+sagarsp123@users.noreply.github.com>
2023-08-01 19:01:10 -0400

    Update app.py

--- app.py
+++ app.py
@@ -756,4 +756,6 @@ def _404Page(str):
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    # app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
